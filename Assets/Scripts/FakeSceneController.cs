8362e618acb8b60dd862a79f25b995c3f0d17c4a
Kortex <jfcontart@idemobi.com>
2019-05-28 10:50:47 +0200

    [STEP] need layout and design

---
+++ Assets/Scripts/FakeSceneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ShowMyApp_API;
+using UnityEngine.SceneManagement;
+
+public class FakeSceneController : MonoBehaviour
+{
+    public ShowMyApp MyShowMyApp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ShareAction()
+    {
+        if (MyShowMyApp != null)
+        {
+            SceneManager.LoadScene("ShowMyAppScene", LoadSceneMode.Additive);
+        }
+    }
+}
